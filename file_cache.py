b870087b223be2386c255ea0c2769405e8157d39
Penn <jenkspt@gmail.com>
2021-02-10 11:01:22 -0800

    initial commit

---
+++ file_cache.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+import functools
+import pickle
+import hashlib
+
+
+def make_key(function, args, kwargs, typed=False):
+    key = args + tuple(kwargs.values())
+    if typed:
+        key = zip(key, map(type, key))
+    return (function.__module__, function.__name__) + tuple(key)
+
+
+def hash_key(key):
+    """ Create stable hash from pickle bytes """
+    key_bytes = pickle.dumps(key)
+    key_hash = hashlib.sha1(key_bytes).hexdigest()
+    return key_hash
+
+def write_cache_file(path, key, data):
+    with open(path, 'wb') as f:
+        pickle.dump((key, data), f)
+
+def read_cache_file(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+
+def cache(path: str='.cache', typed: bool =False):
+    """
+    Creates a persistent local file cache, keyed on the 
+    function and function arguments.
+
+    Args:
+        path: Directory path to store the cache
+        typed: If true, cache on argument types
+    """
+    path = Path(path)
+    path.mkdir(exist_ok=True)
+
+    def decorating_function(user_function):
+
+        @functools.wraps(user_function)
+        def wrapper(*args, **kwargs):
+            cache_bust = kwargs.pop('cache_bust', False)
+            print('Cache Bust?', cache_bust)
+            # Create the identifier from the function and arguments
+            key = make_key(user_function, args, kwargs, typed)
+            # Create a stable hash of the key
+            hashed_key = hash_key(key)
+            
+            cache_file = None 
+            for cache_file in path.glob(f'{hashed_key}_*.pkl'):
+                saved_key, data = read_cache_file(cache_file)
+                # Check for hash collisions
+                if key == saved_key:
+                    if cache_bust:
+                        data = user_function(*args, **kwargs)
+                        write_cache_file(cache_file, key, data)
+                    print('Cache Hit')
+                    return data
+            print('Cache Miss')
+            # Collisions will increment the file suffix
+            n = int(cache_file.stem.split('_')[-1]) + 1 if cache_file else 0
+            data = user_function(*args, **kwargs)
+            write_cache_file(path / f'{hashed_key}_{n}.pkl', key, data)
+            return data
+
+        return wrapper
+
+    return decorating_function
+
+
+if __name__ == "__main__":
+
+    @cache('cache')
+    def get_json(a):
+        return {'name': 'penn'}
+    
+    print(get_json(5))
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pickle-cache',
+      version='0.1',
+      description='Simple persistent local file cache decorator',
+      url='',
+      author='Penn Jenks',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      py_modules=['file_cache'])
